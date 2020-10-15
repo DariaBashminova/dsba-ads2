4cf2f89f00a6ae1b884852dc6b595ac374a6bc43
DariaBashminova <64349075+DariaBashminova@users.noreply.github.com>
2020-10-15 21:31:02 +0300

    Add files via upload

---
+++ ugraph_algos.hpp
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \file
+/// \brief      Contains implementations of some algorithms for undirected graph.
+/// \author     Sergey Shershakov
+/// \version    0.1.0
+/// \date       21.09.2020
+/// \copyright  © Sergey Shershakov 2020.
+///             This code is for educational purposes of the course "Algorithms
+///             and Data Structures" provided by the Faculty of Computer Science
+///             at the Higher School of Economics.
+///
+/// When altering code, a copyright line must be preserved.
+///
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef UGRAPH_ALGOS_HPP
+#define UGRAPH_ALGOS_HPP
+
+#include <set>
+
+#include "lbl_ugraph.hpp"
+
+
+/// Finds a MST for the given graph \a g using Prim's algorithm.
+template<typename Vertex, typename EdgeLbl>
+std::set<typename EdgeLblUGraph<Vertex, EdgeLbl>::Edge>
+    findMSTPrim(const EdgeLblUGraph<Vertex, EdgeLbl>& g)
+{
+    // TODO: implement this!
+
+    std::set<typename EdgeLblUGraph<Vertex, EdgeLbl>::Edge> res;
+    return res;
+}
+
+/// Finds a MST for the given graph \a g using Kruskal's algorithm.
+template<typename Vertex, typename EdgeLbl>
+std::set<typename EdgeLblUGraph<Vertex, EdgeLbl>::Edge>
+    findMSTKruskal(const EdgeLblUGraph<Vertex, EdgeLbl>& g)
+{
+    // TODO: implement this!
+
+    std::set<typename EdgeLblUGraph<Vertex, EdgeLbl>::Edge> res;
+    return res;
+}
+
+
+#endif // UGRAPH_ALGOS_HPP
